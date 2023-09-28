fb5ab4c9553fef2ee9c0e9c16db5231aafec44a7
X-Camille <c.elgueta04@ufromail.cl>
2023-09-28 15:20:14 -0300

    Creación de la clase Biblioteca.java, con sus respectivos atributos, métodos getters y setters, y método constructor.

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -1,9 +1,41 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Biblioteca {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+    private ArrayList<Libro> libros;
+    private String nombreBiblioteca;
+    private String direccionBiblioteca;
+
+    public Biblioteca(ArrayList<Libro> libros, String nombreBiblioteca, String direccionBiblioteca){
+        this.libros = libros;
+        this.nombreBiblioteca = nombreBiblioteca;
+        this.direccionBiblioteca = direccionBiblioteca;
+    }
+
+    public List<Libro> getLibros() {
+        return this.libros;
+    }
+
+    public String getNombreBiblioteca(){
+        return this.nombreBiblioteca;
+    }
+
+    public String getDireccionBiblioteca(){
+        return this.direccionBiblioteca;
+    }
+
+    public void setLibros(ArrayList<Libro> libros){
+        this.libros = libros;
+    }
+
+    public void setNombreBiblioteca(String nombreBiblioteca){
+        this.nombreBiblioteca = nombreBiblioteca;
+    }
+
+    public void setDireccionBiblioteca(String direccionBiblioteca){
+        this.direccionBiblioteca = direccionBiblioteca;
     }
 }
 
